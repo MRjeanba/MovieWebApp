54f04951a704e507afa8c6549a500cde4ae1436b
Jean Baptiste <jeanba19@outlook.fr>
2023-03-24 18:08:13 -0400

    add the closeModalHandler on cancel button of deleteUpdateModal

--- client/src/components/UX/DeleteUpdateModal.js
+++ client/src/components/UX/DeleteUpdateModal.js
@@ -3,14 +3,16 @@ import classes from './DeleteUpdateModal.module.css';
 
 const DeleteUpdateModal = (props) => {
 
+    /**Add logic for delete/update calls here */
+    /**Add the logic to retrieve movie information here(pass from MovieDetails to deleteUpdateModal) */
     return (
         <>
             <BackDrop backdropHandler={props.modalHandler}/>
             <div className={classes.modal}>
-                <h2>The action you wanna perform</h2>
-                <p>The possible outcomes of the action</p>
-                <button>Proceed</button>
-                <button>Cancel</button>
+                <h2 className={classes.title}>The action you wanna perform</h2>
+                <p className={classes.outcomes}>The possible outcomes of the action</p>
+                <button className={classes.modalButton}>Proceed</button>
+                <button className={classes.modalButton} onClick={props.modalHandler}>Cancel</button>
             </div>
         </>
 
